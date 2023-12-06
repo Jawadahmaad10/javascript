8301826abd376b25ef1bd2c63eae64701d2fe67c
Jawad Ahmad <75661855+Jawadahmaad10@users.noreply.github.com>
2023-12-06 19:03:03 +0000

    js

---
+++ basics/typeconversion.js
@@ -0,0 +1,20 @@
+let score = '33'
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+
+let value = Number(score)
+
+console.log(value);
+console.log(typeof(value));
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let someNumver = 55
+let stringNumber = String(someNumver)
+console.log(typeof stringNumber)
